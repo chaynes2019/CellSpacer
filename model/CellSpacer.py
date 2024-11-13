5fd107311f7a6944bf0322ea91c6539e2794c0d2
Cameron Haynes <chaynes2019@gmail.com>
2024-11-13 11:10:48 -0500

    Re-make original cancerous growth animation with time tracking

--- model/CellSpacer.py
+++ model/CellSpacer.py
@@ -13,7 +13,7 @@ yLength = 101
 defaultMu = 0
 defaultOmega = 85
 
-iterations = 420
+iterations = 210
 
 gridHistory = np.zeros((xLength, yLength, iterations))
 
@@ -26,6 +26,7 @@ for t in range(iterations):
     color_matrix = cellVisual.computeMatrixFromGrid(grid)
     gridHistory[:, :, t] = color_matrix
 
+    """
     if t == 30:
         grid.treatWithTelomeraseInhibitor()
 
@@ -38,19 +39,20 @@ for t in range(iterations):
                               0.75)
         
         grid.spaces[(50, 50)].setOccupant(cell_to_mutate)
+    """
 
     grid.tick()
 
 fig, ax = plt.subplots()
 
 displayMatrix = ax.matshow(gridHistory[:, :, 0])
-ax.set_title(f"Cancer treated at t = 30 out of 420: mu = {-1}, omega = {defaultOmega}, t = 0")
+ax.set_title(f"Labeled Single Cancer Cell: mu = {-1}, omega = {defaultOmega}, tau = 1, t = 0")
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 
 def animate(t):
     displayMatrix.set_array(gridHistory[:, :, t])
-    ax.set_title(f"Cancer treated at t = 30 out of 420: mu = {-1}, omega = {defaultOmega}, t = {t}")
+    ax.set_title(f"Labeled Single Cancer Cell: mu = {-1}, omega = {defaultOmega}, tau = 1, t = {t}")
     return [displayMatrix]
 
 anim = animation.FuncAnimation(fig, func = animate, frames = iterations, interval = 30)
@@ -59,9 +61,9 @@ anim = animation.FuncAnimation(fig, func = animate, frames = iterations, interva
 
 mpegWriter = animation.FFMpegWriter(fps = 30)
 
-anim.save("resistantEscapeCancerousTissueGrowth.mp4", writer = mpegWriter)
+anim.save("/home/hytendf/Projects/IMOYear1/CellSpacer/OutputVideosAndGifs/labeledSingleCellCancerousTissueGrowth.mp4", writer = mpegWriter)
 
 gifWriter = animation.PillowWriter(fps = 30)
-anim.save("resistantEscapeCancerousTissueGrowth.gif", writer = gifWriter)
+anim.save("/home/hytendf/Projects/IMOYear1/CellSpacer/OutputVideosAndGifs/labeledSingleCellCancerousTissueGrowth.gif", writer = gifWriter)
 
 exit()
