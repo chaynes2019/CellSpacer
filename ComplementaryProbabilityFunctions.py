28367ea7dd69f557476089afffcb973b6f4cce5b
Cameron Haynes <chaynes2019@gmail.com>
2024-11-15 13:34:01 -0500

    Rework model probabilities to be based on organism fitness calculated from environment coincidence

--- ComplementaryProbabilityFunctions.py
+++ ComplementaryProbabilityFunctions.py
@@ -1,13 +1,13 @@
 probDoNothing = 0.0
 
-def p_r(deltaPhi):
-    return (1 - probDoNothing) * (1 - deltaPhi)
+def p_r(fitness):
+    return (1 - probDoNothing) * (fitness)
 
-def p_q(deltaPhi):
-    return (1 - probDoNothing) * (deltaPhi) / 2
+def p_q(fitness):
+    return (1 - probDoNothing) * (1 - fitness) / 2
 
-def p_a(deltaPhi):
-    return (1 - probDoNothing) * (deltaPhi) / 2
+def p_a(fitness):
+    return (1 - probDoNothing) * (1 - fitness) / 2
 
-def p_n(deltaPhi):
-    return 1 - deltaPhi
+def p_n(fitness):
+    return fitness
